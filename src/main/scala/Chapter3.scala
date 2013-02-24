d575fc939cce74d1a132d6f68aa43620e5451ef6
Dmitry Bushev <bushevdv@gmail.com>
2013-02-24 19:33:01 +0400

    Chapter 3 completed

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -95,8 +95,139 @@ trait Chapter3 {
     def reverse[A](l: List[A]): List[A] =
       foldLeft(l, Nil: List[A])((acc, h) => Cons(h, acc))
 
+    // Excercise 14
+    def foldLeftR[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+      foldRight(reverse(l), z)((acc, h) => f(h, acc))
+
+    def foldRightL[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+      foldLeft(reverse(l), z)((h, acc) => f(acc, h))
+
+    // Excercise 15
+    def append[A](l1: List[A], l2: List[A]): List[A] =
+      foldRight(l1, l2)(Cons.apply)
+
+    // Excercise 16
+    def flatten[A](ll: List[List[A]]): List[A] =
+      foldRight(ll, Nil: List[A])(append)
+
+    // Excercise 17
+    def map_++(l: List[Int]): List[Int] = l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(h + 1, map_++(t))
+    }
+
+    // Excercise 18
+    def map_toString(l: List[Double]): List[String] = l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(h.toString, map_toString(t))
+    }
+
+    // Excercise 19
+    def map[A, B](l: List[A])(f: A => B): List[B] = l match {
+      case Nil => Nil
+      case Cons(h, t) => Cons(f(h), map(t)(f))
+    }
+
+    // Excercise 20
+    def filter[A](l: List[A])(p: A => Boolean): List[A] = l match {
+      case Nil => Nil
+      case Cons(h, t) =>
+        if (p(h)) Cons(h, filter(t)(p))
+        else filter(t)(p)
+    }
+
+    // Excercise 21
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
+      case Nil => Nil
+      case Cons(h, t) => append(f(h), flatMap(t)(f))
+    }
+
+    // Excercise 22
+    def filterf[A](l: List[A])(p: A => Boolean): List[A] =
+      flatMap(l)(x => if (p(x)) List(x) else Nil)
+
+    // Excercise 23
+    def zip_+(l1: List[Int], l2: List[Int]): List[Int] = (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, zip_+(t1, t2))
+    }
+
+    // Excercise 24
+    def zip[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zip(t1, t2)(f))
+    }
+
+    // Excercise 25
+    def forall[A](l: List[A])(p: A => Boolean): Boolean = l match {
+      case Nil => true
+      case Cons(h, t) => p(h) && forall(t)(p)
+    }
+
+    def hasSubsequence[A](l: List[A], sub: List[A]): Boolean = l match {
+      case Nil => false
+      case Cons(h, t) => forall(zip(l, sub)(_ == _))(identity) || hasSubsequence(t, sub)
+    }
   }
 
+  sealed trait Tree[+A]
+  case class Leaf[A](value: A) extends Tree[A]
+  case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  object Tree {
+    val example =
+      Branch(
+        Branch(Leaf(1), Leaf(2)),
+        Branch(
+          Branch(Leaf(3), Leaf(4)),
+          Leaf(5)
+        )
+      )
+
+    // Excercise 26
+    def size[A](t: Tree[A]): Int = t match {
+      case Leaf(_) => 1
+      case Branch(l, r) => 1 + size(l) + size(r)
+    }
+
+    // Excercise 27
+    def maximum(t: Tree[Int]): Int = t match {
+      case Leaf(v) => v
+      case Branch(l, r) => maximum(l) max maximum(r)
+    }
+
+    // Excercise 28
+    def depth[A](t: Tree[A]): Int = t match {
+      case Leaf(_) => 0
+      case Branch(l, r) => 1 + (depth(l) max depth(r))
+    }
+
+    // Excercise 29
+    def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
+
+    // Excercise 30
+    def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+      case Leaf(v) => f(v)
+      case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+    }
+
+    def sizef[A](t: Tree[A]): Int =
+      fold(t)(_ => 1)(1 + _ + _)
+
+    def maxf(t: Tree[Int]): Int =
+      fold(t)(identity)(_ max _)
+
+    def depthf[A](t: Tree[A]): Int =
+      fold(t)(_ => 0)((l, r) => 1 + (l max r))
+
+    def mapf[A, B](t: Tree[A])(f: A => B): Tree[B] =
+      fold(t)(v => Leaf(f(v)): Tree[B])(Branch.apply)
+  }
 }
 
 object ch3 extends Chapter3
